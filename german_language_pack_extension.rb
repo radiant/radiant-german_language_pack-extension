cecaf149a9d173e3e8127ad01075efa5d050dc46
Simon Zöllner <mail@simonzoellner.de>
2013-08-30 15:50:15 +0200

    Link to GitHub instead of a dummy website

--- german_language_pack_extension.rb
+++ german_language_pack_extension.rb
@@ -1,7 +1,7 @@
 class GermanLanguagePackExtension < Radiant::Extension
   version "1.0"
   description "Provides a German translation for the Radiant admin interface"
-  url "http://yourwebsite.com/de"
+  url "https://github.com/radiant/radiant-german_language_pack-extension"
 
   def activate
 
